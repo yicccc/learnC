f114eb213732c935a2f102c84bec7f68149d9c38
yicccc <yicccc7@gmail.com>
2016-03-03 11:28:33 +0800

    add ss.sh

---
+++ ss.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cat << ss >./dhcpd6.conf
+default-lease-time 1800;
+max-lease-time 7200;
+
+subnet6 ${base_prefix}00::/64 {
+  prefix6 ${base_prefix}a0:: ${base_prefix}f0:: /60;
+}
+ss
